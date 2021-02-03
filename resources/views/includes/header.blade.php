5888d01a020e79a5a9e22cb44d8db5ce68dbce62
javin4 <florian@waldmayer.com>
2021-02-03 21:00:29 +0100

    first commit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,6 +2,8 @@
     <div class="navbar-inner">
         <br>
         <a href="/"> Home </a> |
+        <a href="/project"> project </a> |
+        <a href="/lv"> lv </a> |
         <a href="/companies"> companies </a> |
         <a href="/responses"> responses </a>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/', function () { return View::make('pages.home'); });
 Route::get('/companies', function () { return View::make('pages.companies'); });
 Route::get('/responses', function () { return View::make('pages.responses'); });
+Route::get('/project', function () { return View::make('pages.project'); });
+Route::get('/lv', function () { return View::make('pages.lv'); });
