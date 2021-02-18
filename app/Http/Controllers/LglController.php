22e633e0c739e65ad8a2096f88c90af7698baa00
javin4 <florian@waldmayer.com>
2021-02-18 09:15:44 +0100

    initial commit

---
+++ app/Http/Controllers/LglController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\gl\Lgl;
+use Illuminate\Http\Request;
+
+class LglController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\gl\Lgl  $lgl
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lgl $lgl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\gl\Lgl  $lgl
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lgl $lgl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\gl\Lgl  $lgl
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lgl $lgl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\gl\Lgl  $lgl
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lgl $lgl)
+    {
+        //
+    }
+}
---
+++ database/factories/Gl/LglFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\gl;
+
+use App\Models\gl\Lgl;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LglFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Lgl::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
